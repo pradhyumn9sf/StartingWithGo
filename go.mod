da563164b95717ec91f04e5250845cab92acf70f
“Pradhyumn <prathore@splunk.com>
2025-04-11 12:14:23 +0530

    using first tag commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/pradhyumn9sf/StartingWithGo
+
+go 1.22.4
+
+require github.com/pradhyumn9sf/Puppy v0.0.0-20250411062339-568aa81f5907
+
+require github.com/pradhyumn9sf/Dog v0.0.0-20250411062047-f443c08bcedd // indirect
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pradhyumn9sf/Puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+	fmt.Println("Hello Pradhyumn 😁")
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	fmt.Println(s4)
+
+}
